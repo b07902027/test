83a1963b2a8fff6d2781b63345e54fe3efeba837
b07902027 <b07902027@ntu.edu.tw>
2019-06-09 13:56:00 +0800

    v2.0 using std::string as return

--- lcs.cpp
+++ lcs.cpp
@@ -1,50 +1,26 @@
-//#include <cstdio>
-#include <iostream>
-#include <vector>
+#include <cstdio>
+#include <string>
 #define MAXN 10001
-typedef struct{
-    bool test;
-    char c;
-    short l;
-}Data;
 char A[MAXN], B[MAXN];
-std::string ans;
 
-Data lcs(int N, int M){
-    Data d;
+std::string lcs(int N, int M){
     if (!N || !M){
-        d.l = d.test = 0;
-        printf("%d %d, return d l = %d, test = %d, c = %c\n",N,M, d.l, d.test, d.c);
-        return d;
+        std::string ans("");
+        return ans;
     }
     if (A[N - 1] == B[M - 1]){
-        d.test = 1;
-        d.c = A[N - 1];
-        Data ansd = lcs(N - 1, M - 1);
-        d.l = ansd.l + 1;
-        if (ansd.test){
-            ans.push_back(ansd.c);
-        }
-        printf("%d %d, return d l = %d, test = %d, c = %c\n",N,M, d.l, d.test, d.c);
-        return d;
+        std::string ans = lcs(N - 1, M - 1);
+        ans.push_back(A[N - 1]);
+        return ans;
     }else{
-        d.test = 0;
-        Data ansA, ansB;
+        std::string ansA, ansB;
         ansA = lcs(N - 1, M);
         ansB = lcs(N, M - 1);
-        if (ansA.l > ansB.l){
-            d.l = ansA.l;
-            if (ansA.test){
-                ans.push_back(ansA.c);
-            }
+        if (ansA.length() > ansB.length()){
+            return ansA;
         }else{
-            d.l = ansB.l;
-            if (ansB.test){
-                ans.push_back(ansB.c);
-            }
+            return ansB;
         }
-        printf("%d %d, return d l = %d, test = %d, c = %c\n",N,M, d.l, d.test, d.c);
-        return d;
     }
 }
 
@@ -54,11 +30,15 @@ int main(){
     while(T--){
         scanf("%d%d",&N,&M);
         scanf("%s%s",A,B);
-        ans.clear();
-        Data d = lcs(N, M);
-        if (d.test){
-            ans.push_back(d.c);
+        std::string ans = lcs(N, M);
+        if (ans.empty()){
+            printf("*\n");
+        }else{
+            int l = ans.length();
+            for (int i = 0; i < l; i++){
+                printf("%c",ans[i]);
+            }
+            printf("\n");
         }
-        std::cout << ans;
     }
 }
